1fa31b901ffaf6c30bc9fffca924eec4021d2dd8
Carlos Melgarejo <carlos.melgarejo.solorzano@hotmail.com>
2023-03-17 11:55:30 -0500

    adads

--- platos.js
+++ platos.js
@@ -4,4 +4,8 @@ let bocaditos;
 =======
 let bocaditos = "Arroz conleche"
 let perrito = "tommy"
+<<<<<<< HEAD
 >>>>>>> 077e351 (agregando a tommy)
+=======
+let perrita = "añadiendo sandia!"
+>>>>>>> b4b2bac (añadiendo saniiito)
