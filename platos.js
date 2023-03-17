6a3f1d39da312a70b1899305aa5e0b01fb8757a6
Carlos Melgarejo <carlos.melgarejo.solorzano@hotmail.com>
2023-03-17 11:59:53 -0500

    agregando sec

--- platos.js
+++ platos.js
@@ -1,11 +1,4 @@
 let platos = ["Ceviche"];
-<<<<<<< HEAD
-let bocaditos;
-=======
-let bocaditos = "Arroz conleche"
-let perrito = "tommy"
-<<<<<<< HEAD
->>>>>>> 077e351 (agregando a tommy)
-=======
-let perrita = "añadiendo sandia!"
->>>>>>> b4b2bac (añadiendo saniiito)
+
+
+let secuna;
